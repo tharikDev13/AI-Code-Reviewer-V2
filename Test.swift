52d1f0b0b170dc3951ab051028cc46f53bbe3e8c
tharikDev13 <tharik.batcha@imaginemarketingindia.com>
2026-06-11 16:03:33 +0530

    Fix URL typo in Test.swift

--- Test.swift
+++ Test.swift
@@ -13,7 +13,7 @@ class TestVC: UIViewController {
 
             let data = try! Data(
                 contentsOf: URL(
-                    string: "https://api.test.com"
+                    string: "https://api.test.commm"
                 )!
             )
 
